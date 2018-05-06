2064b31feea486ecc39cda091a4fc1355fed4f1c
Korna <artem-koma@yandex.ru>
2018-05-06 21:12:18 +0300

    add connect and queue messages

--- sender.c
+++ sender.c
@@ -37,29 +37,52 @@ int main(){
 	addr.sin_port = htons(3425);
 	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
+	sendto(sock, "Con", sizeof("Con"), 0,
+	(struct sockaddr *)&addr, sizeof(addr));
+	
 	while(1){
-		    printf("Input your string:");
-		fgets(msg1, 1000, stdin);
-	
-		sendto(sock, msg1, sizeof(msg1), 0,
-	
-		(struct sockaddr *)&addr, sizeof(addr));
-	
-		connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-	
-	//	send(sock, msg2, sizeof(msg2), 0);
-		if(command_exit(msg1)){
-			printf("Exitting...\n");
-			break;
-		}else{
 			if((recvfrom(sock, recvline, 1000, 0, 
 		    (struct sockaddr *) 0, 0)) < 0){
-		        perror("socket");
+		        perror("Receive error:");
 		        close(sock);
 		        exit(1);
-		    }
-		    printf("Answer from server:%s\n", recvline);
+		    }else
+		    	printf("Server:%s\n", recvline);
+		    	
+		    if(recvline[0] == '0')
+		    	break;
+		    	
+	}
+
+
+	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) >= 0)
+		while(1){
+			printf("Input your string:");
+			fgets(msg1, 1000, stdin);
+		
+			sendto(sock, msg1, sizeof(msg1), 0,
+			(struct sockaddr *)&addr, sizeof(addr));
+		
+			
+		
+		//	send(sock, msg2, sizeof(msg2), 0);
+			if(command_exit(msg1)){
+				printf("Exitting...\n");
+				break;
+			}else{
+				if((recvfrom(sock, recvline, 1000, 0, 
+			    (struct sockaddr *) 0, 0)) < 0){
+			        perror("Receive error:");
+			        close(sock);
+			        exit(1);
+			    }else
+			    	printf("Answer from server:%s\n", recvline);
+			}
 		}
+	else{
+		perror("Connect error:");
+		close(sock);
+		exit(1);
 	}
 	
 	close(sock);

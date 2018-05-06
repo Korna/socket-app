8124e480ff6c4a9186c25f9d39d4bff0eafcd234
Korna <artem-koma@yandex.ru>
2018-05-06 19:46:14 +0300

    add server  with first connected client tracking

---
+++ sender.c
@@ -0,0 +1,67 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <string.h>
+#include <stdio.h>
+//#include <conio.h>
+
+char message_exit[] = "exit\0";
+
+char msg1[] = "Hello there!\n";
+char msg2[] = "Bye bye!\n";
+
+int command_exit(char* msg){
+	if(msg[0] == 'e' &&
+	msg[1] == 'x' &&
+	msg[2] == 'i' &&
+	msg[3] == 't')
+		return 1;
+	else 
+		return 0;
+}
+
+
+int main(){
+	char recvline[1000];
+	int sock;
+	struct sockaddr_in addr;
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if(sock < 0){
+		perror("socket");
+		exit(1);
+	}
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	while(1){
+		    printf("Input your string:");
+		fgets(msg1, 1000, stdin);
+	
+		sendto(sock, msg1, sizeof(msg1), 0,
+	
+		(struct sockaddr *)&addr, sizeof(addr));
+	
+		connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+	
+	//	send(sock, msg2, sizeof(msg2), 0);
+		if(command_exit(msg1)){
+			printf("Exitting...\n");
+			break;
+		}else{
+			if((recvfrom(sock, recvline, 1000, 0, 
+		    (struct sockaddr *) 0, 0)) < 0){
+		        perror("socket");
+		        close(sock);
+		        exit(1);
+		    }
+		    printf("Answer from server:%s\n", recvline);
+		}
+	}
+	
+	close(sock);
+	return 0;
+}
